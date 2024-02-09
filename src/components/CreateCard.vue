91f7cde8de6d78aaa4716f0af36dc5d1d7b12949
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-02-09 19:41:26 +0100

    add card header

--- src/components/CreateCard.vue
+++ src/components/CreateCard.vue
@@ -8,10 +8,17 @@
   </button>
 
   <div v-show="store.cardInputOpen">
+    <input
+      type="text"
+      id="card-header"
+      placeholder="card header"
+      class="border border-gray-400"
+      v-model="inputCardHeader"
+    />
     <textarea
       id="card-text"
       placeholder="write down some text"
-      class="w-full h-20 border-2 border-black"
+      class="w-full h-20 border border-gray-400"
       v-model="inputCardText"
     ></textarea>
 
@@ -19,7 +26,7 @@
       <input
         type="text"
         placeholder="type in category name"
-        class="w-1/5 h-10 border-2 border-black"
+        class="w-1/5 h-10 border border-gray-400"
         v-model.lazy="inputCreateNewCategory"
       />
       <button @click="saveNewCategory()">save category</button>
@@ -49,11 +56,11 @@
     <button
       v-show="inputCardText && inputCardCategory"
       v-on:click="onSave"
-      class="border-2 border-black"
+      class="border border-gray-400"
     >
       Save
     </button>
-    <button v-on:click="onCancel" class="border-2 border-black">Cancel</button>
+    <button v-on:click="onCancel" class="border border-gray-400">Cancel</button>
   </div>
 </template>
 
@@ -70,6 +77,7 @@ export default {
   },
   data() {
     return {
+      inputCardHeader: '',
       inputCardText: '',
       inputCardCategory: '',
       showCategories: false,
@@ -81,6 +89,7 @@ export default {
     'store.cardToEdit': {
       handler() {
         if (this.store.cardToEdit) {
+          this.inputCardHeader = this.store.cardToEdit.cardHeader
           this.inputCardText = this.store.cardToEdit.cardText
           this.inputCardCategory = this.store.cardToEdit.category
         }
@@ -112,24 +121,35 @@ export default {
     },
 
     onSave() {
-      if (this.inputCardCategory && this.inputCardText) {
+      if (this.inputCardHeader && this.inputCardCategory && this.inputCardText) {
         const timestamp = new Date().toISOString()
 
         if (Object.keys(this.store.cardToEdit).length === 0) {
           this.store.totalCards++
 
           this.saveMemoryCard(timestamp)
-          this.saveCardToFirebase(this.inputCardText, this.inputCardCategory, timestamp)
+          this.saveCardToFirebase(
+            this.inputCardHeader,
+            this.inputCardText,
+            this.inputCardCategory,
+            timestamp
+          )
         } else
           this.saveCardToFirebase(
+            this.inputCardHeader,
             this.inputCardText,
             this.inputCardCategory,
             this.store.cardToEdit.timestamp,
             this.store.cardToEdit.cardId
           ),
-            this.updateCardInStore(this.inputCardText, this.inputCardCategory),
-            (this.inputCardText = '')
-        this.inputCardCategory = ''
+            this.updateCardInStore(
+              this.inputCardHeader,
+              this.inputCardText,
+              this.inputCardCategory
+            ),
+            (this.inputCardHeader = ''),
+            (this.inputCardText = ''),
+            (this.inputCardCategory = '')
       }
       this.store.cardInputOpen = false
     },
@@ -138,6 +158,7 @@ export default {
       if (this.inputCardCategory && this.inputCardText) {
         window.confirm('Are you sure you want to cancel? All changes will be lost.')
       }
+      this.inputCardHeader = ''
       this.inputCardText = ''
       this.inputCardCategory = ''
       this.store.cardToEdit = {}
@@ -146,6 +167,7 @@ export default {
 
     saveMemoryCard(timestamp) {
       this.store.cards.push({
+        cardHeader: this.inputCardHeader,
         cardText: this.inputCardText,
         category: this.inputCardCategory,
         timestamp: timestamp,
