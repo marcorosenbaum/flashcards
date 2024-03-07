91c9c95ebfdaa9357b976aaa06894e02a9ef4f18
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-03-07 21:27:00 +0100

    add more icons to vite config

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,19 @@ export default defineConfig({
       // Add the module as an external dependency
       external: [
         'vue-material-design-icons/trashCanOutline.vue',
-        'vue-material-design-icons/FormatStrikeThrough.vue'
+        'vue-material-design-icons/FormatStrikeThrough.vue',
+        'vue-material-design-icons/FormatBold.vue',
+        'vue-material-design-icons/FormatItalic.vue',
+        'vue-material-design-icons/FormatStrikeThrough.vue',
+        'vue-material-design-icons/CodeTags.vue',
+        'vue-material-design-icons/FormatUnderline.vue',
+        'vue-material-design-icons/Minus.vue',
+        'vue-material-design-icons/CodeBlockTags.vue',
+        'vue-material-design-icons/FormatHeader1.vue',
+        'vue-material-design-icons/FormatHeader2.vue',
+        'vue-material-design-icons/FormatListBulleted.vue',
+        'vue-material-design-icons/FormatListNumbered.vue',
+        'vue-material-design-icons/CommentQuoteOutline.vue'
       ]
     }
   }
