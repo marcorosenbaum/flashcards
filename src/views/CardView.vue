ad5a6d9cd3231116268046d67f173ddf63308041
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-03-07 13:33:53 +0100

    add styling

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -1,55 +1,29 @@
 <template>
   <router-link v-show="Object.keys(store.cardToEdit).length === 0" :to="{ name: 'home' }">
-    click HERE to go back
+    <arrow-left size="30" class="text-orange-300"></arrow-left>
   </router-link>
 
   <create-card></create-card>
 
-  <section class="p-4 border flex-wrap rounded-xl bg-light-navy flex justify-between">
+  <section class="p-4 border rounded-xl bg-light-navy flex justify-between">
     <div class="prose">
       <h1 class="underline">{{ currentCard.cardHeader }}</h1>
       <div v-html="currentCard.cardText"></div>
-      <span class="text-green-500">{{ currentCard.category }}</span>
-      <span class="text-purple-500">{{ currentCard.cardId }}</span>
-      <span class="text-blue-500">{{ currentCard.timestamp }}</span>
+      <span class="text-orange-300 mr-4">{{ currentCard.category }}</span>
+
+      <span class="text-blue-500"
+        >Date
+        {{
+          currentCard.timestamp.length > 10
+            ? currentCard.timestamp.substring(0, 10)
+            : currentCard.timestamp
+        }}</span
+      >
     </div>
 
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-trash-2"
-        @click="deleteCard(currentCard)"
-      >
-        <path d="M3 6h18" />
-        <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-        <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-        <line x1="10" x2="10" y1="11" y2="17" />
-        <line x1="14" x2="14" y1="11" y2="17" />
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-pencil"
-        @click="enableCardEdit(currentCard)"
-      >
-        <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-        <path d="m15 5 4 4" />
-      </svg>
+    <div class="flex flex-col gap-6">
+      <delete-icon size="30" @click="deleteCard(currentCard)"></delete-icon>
+      <edit-icon size="30" @click="enableCardEdit(currentCard)"></edit-icon>
     </div>
   </section>
 </template>
@@ -57,6 +31,9 @@
 <script>
 import useUserStore from '@/stores/users.js'
 import CreateCard from '@/components/CreateCard.vue'
+import ArrowLeft from 'vue-material-design-icons/ArrowLeft.vue'
+import DeleteIcon from 'vue-material-design-icons/trashCanOutline.vue'
+import EditIcon from 'vue-material-design-icons/pencil.vue'
 
 export default {
   name: 'CardView',
@@ -66,7 +43,10 @@ export default {
     }
   },
   components: {
-    CreateCard
+    CreateCard,
+    ArrowLeft,
+    DeleteIcon,
+    EditIcon
   },
   data() {
     return {
