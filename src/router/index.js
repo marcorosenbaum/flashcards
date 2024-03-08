19859cd0903ed24b7d5a83715c65f528d08947df
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2024-03-08 09:15:26 +0100

    add base to router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(process.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -19,7 +20,8 @@ const router = createRouter({
       name: 'learn',
       component: () => import('../views/LearnView.vue')
     }
-  ]
+  ],
+  base: '/'
 })
 
 export default router
